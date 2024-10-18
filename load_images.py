427210e54fafd4f8f101028bd498f94d1f8af8e2
garlic0000 <garlic0000@163.com>
2024-10-18 09:18:29 +0800

    Update load_images.py

--- load_images.py
+++ load_images.py
@@ -14,70 +14,93 @@ dir_crop_root_path = "/kaggle/working/rawpic_crop"
 
 
 def get_rawpic_count(root_path):
+    """
+    递归地统计数据集中所有 .jpg 图片的数量
+    遍历所有子目录，最终返回图片总数
+    Args:
+        root_path: 图片根目录
+
+    Returns:图片总数
+    """
     count = 0
-    for sub_item in Path(root_path).iterdir():
-        if sub_item.is_dir():
-            for type_item in sub_item.iterdir():
-                if type_item.is_dir():
+    for sub in Path(root_path).iterdir():
+        if sub.is_dir():
+            for vid in sub.iterdir():
+                if vid.is_dir():
                     count += len(glob.glob(os.path.join(
-                        str(type_item), "*.jpg")))
+                        str(vid), "*.jpg")))
     return count
 
 
 def crop_images(dataset_name):
     """
-
+    对数据集中的图像进行裁剪，提取出人脸区域并保存成新图片
+    使用 dlib 的 HOG 面部检测器来检测每张图片中的人脸
+    对人脸进行裁剪和缩放
+    裁剪后的图片以 128x128 的分辨率保存
     Args:
-        dataset_name:
+        dataset_name:处理的数据集名称
 
-    Returns:
+    Returns: 已裁剪和缩放的图片
 
     """
+    # 加载 HOG 人脸检测器
     face_detector = dlib.get_frontal_face_detector()
-    if (dataset_name == 'CASME_sq'):
+    if dataset_name == 'CASME_sq':
         sum_count = get_rawpic_count(CASME_sq_rawpic_root_path)
         print("rawpic count = ", sum_count)
-        with tqdm(total=sum_count) as tq:
-            # Save the images into folder 'rawpic_crop'
-            for subjectName in glob.glob(CASME_sq_rawpic_root_path + '/*'):
-                dataset_rawpic = CASME_sq_rawpic_root_path + '/' + str(subjectName.split('/')[-1]) + '/*'
+        with tqdm(total=sum_count) as tq:  # 进度条
+            # s15, s16, s19等subject目录名称
+            for sub in Path(CASME_sq_rawpic_root_path).iterdir():
+                if not sub.is_dir():
+                    continue
+                # 把裁剪的图片保存至 'rawpic_crop'
+                # 创建新目录 'rawpic_crop'
+                if not os.path.exists(dir_crop_root_path):
+                    os.mkdir(dir_crop_root_path)
 
-                # Create new directory for 'rawpic_crop'
-                dir_crop = dir_crop_root_path
-                if os.path.exists(dir_crop) == False:
-                    os.mkdir(dir_crop)
-
-                # Create new directory for each subject
-                dir_crop_sub = dir_crop_root_path + '/' + str(subjectName.split('/')[-1]) + '/'
+                # 为每一个subject创建目录
+                dir_crop_sub = os.path.join(dir_crop_root_path, sub.name)
                 if os.path.exists(dir_crop_sub):
                     shutil.rmtree(dir_crop_sub)
                 os.mkdir(dir_crop_sub)
-                print('Subject', subjectName.split('/')[-1])
-                for vid in glob.glob(dataset_rawpic):
-                    dir_crop_sub_vid = dir_crop_sub + vid.split('/')[-1]  # Get dir of video
+                print('Subject', sub.name)
+                for vid in sub.iterdir():
+                    if not vid.is_dir():
+                        continue
+                    print("Video", vid.name)
+                    # 为每段视频创建目录
+                    dir_crop_sub_vid = os.path.join(dir_crop_sub, vid.name)
                     if os.path.exists(dir_crop_sub_vid):
                         shutil.rmtree(dir_crop_sub_vid)
                     os.mkdir(dir_crop_sub_vid)
-                    for dir_crop_sub_vid_img in natsort.natsorted(glob.glob(vid + '/img*.jpg')):  # Read images
-                        img = dir_crop_sub_vid_img.split('/')[-1]
-                        count = img[3:-4]  # Get img num Ex 001,002,...,2021
-                        # Load the image
+                    # natsort 是一个第三方库，用于执行“自然排序”，
+                    # 也就是按人类习惯的方式进行排序。
+                    # 例如，按自然顺序，img2.jpg 会排在 img10.jpg 前面，而不是后面。
+                    dir_crop_sub_vid_img_list = glob.glob(os.path.join(str(vid), "*.jpg"))
+                    # 读取每张图片
+                    for dir_crop_sub_vid_img in natsort.natsorted(dir_crop_sub_vid_img_list):
+                        img = os.path.basename(dir_crop_sub_vid_img)  # 获取文件名，例如 'img001.jpg'
+                        img_name = img[3:-4]  # 获取 '001'
+                        # 读入图片
                         image = cv2.imread(dir_crop_sub_vid_img)
-                        # Run the HOG face detector on the image data
+                        # 运行HOG人脸检测器
                         detected_faces = face_detector(image, 1)
 
-                        if (count == '001'):  # Use first frame as reference frame
+                        if img_name == '001':
+                            # 使用第一帧（图片名为 001）的面部作为参考框架来确定面部的裁剪边界
+                            # 后续帧中将使用同样的边界
                             for face_rect in detected_faces:
                                 face_top = face_rect.top()
                                 face_bottom = face_rect.bottom()
                                 face_left = face_rect.left()
                                 face_right = face_rect.right()
 
-                        face = image[face_top:face_bottom, face_left:face_right]  # Crop the face region
-                        face = cv2.resize(face, (128, 128))  # Resize to 128x128
-
-                        cv2.imwrite(dir_crop_sub_vid + "/img{}.jpg".format(count), face)
-                        tq.update()
+                        face = image[face_top:face_bottom, face_left:face_right]  # 裁剪人脸区域
+                        face = cv2.resize(face, (128, 128))  # 调整尺寸为 128x128
+                        # 保存图片
+                        cv2.imwrite(os.path.join(dir_crop_sub_vid, "img{}.jpg").format(img_name), face)
+                        tq.update()  # 更新进度
 
     # 地址还没进行修改
     elif (dataset_name == 'SAMMLV'):
